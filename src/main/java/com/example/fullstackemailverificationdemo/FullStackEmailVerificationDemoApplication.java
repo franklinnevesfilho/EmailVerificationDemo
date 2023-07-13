b6c359f0a980d382852c94d4ed29c6a5df2a4ca8
franklinnevesfilho <franklin.neves.filho@gmail.com>
2023-07-13 16:50:21 -0400

    init

---
+++ src/main/java/com/example/fullstackemailverificationdemo/FullStackEmailVerificationDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.fullstackemailverificationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FullStackEmailVerificationDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FullStackEmailVerificationDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/fullstackemailverificationdemo/FullStackEmailVerificationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fullstackemailverificationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FullStackEmailVerificationDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
