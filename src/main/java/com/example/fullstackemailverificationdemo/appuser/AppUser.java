27e57c0fe686244608542272c74281304e55260f
Franklin <franklin.neves.filho@gmail.com>
2023-07-13 21:53:57 -0400

    UserApp package intro 7/13/23 @ 9:53

---
+++ src/main/java/com/example/fullstackemailverificationdemo/appuser/AppUser.java
@@ -0,0 +1,51 @@
+package com.example.fullstackemailverificationdemo.appuser;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+
+public class AppUser implements UserDetails {
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private AppUserRole role;
+    private boolean isLocked;
+    private Boolean isEnabled;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return null;
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return false;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return false;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return false;
+    }
+}
---
+++ src/main/java/com/example/fullstackemailverificationdemo/appuser/AppUserRole.java
@@ -0,0 +1,7 @@
+package com.example.fullstackemailverificationdemo.appuser;
+
+public enum AppUserRole {
+    USER,
+    ADMIN
+}
+
